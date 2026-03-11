665179795562982803e4d729af15c7a05d911ceb
VanTho <ovantho094@gmail.com>
2026-03-11 18:15:44 +0700

    mapping reservation

--- client/types/Reservation.ts
+++ client/types/Reservation.ts
@@ -3,17 +3,17 @@ export interface Reservation {
     tableId: number;
     numberOfPeople: number;
     note: string;
-    time: string; // ISO DateTime string
-    date: string; // ISO DateTime string
+    time: string; // ISO 8601 string from C# DateTime
+    date: string; // ISO 8601 string from C# DateTime
     customerName: string;
 }
 
 export interface ReservationCreateDTO {
-    reservationId?: number;
+    reservationId?: number; // Optional on create
     tableId: number;
     numberOfPeople: number;
     note: string;
-    time: string | null;
-    date: string | null;
+    time: string;
+    date: string;
     customerName: string;
 }
